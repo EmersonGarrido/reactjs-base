35d19b08738ae86cd15733e47494f399677180a7
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-07-05 09:36:06 -0400

    chore: update

--- reactjs/src/routes/index.tsx
+++ reactjs/src/routes/index.tsx
@@ -9,7 +9,7 @@ const Routes = (): JSX.Element => {
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Edit <code>src/App.tsx</code> and save to reload.
+                Funcionou
               </p>
               <a
                 className="App-link"
